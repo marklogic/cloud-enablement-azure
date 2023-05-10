feede59f175ec46c98bc7cbca678aba6db6dfe29
Vitaly <vitaly.korolev@gmail.com>
2023-05-10 13:09:41 -0700

    Port over changes from 11 branch

--- scripts/config-bootstrap-node.sh
+++ scripts/config-bootstrap-node.sh
@@ -13,6 +13,8 @@ source ./init.sh $1 "$2" $3 $4 $5
 # variables
 SEC_REALM=$6
 BOOTSTRAP_HOST=$7
+N_RETRY=10
+RETRY_INTERVAL=10
 
 ######################################################################################################
 # Bring up the first host in the cluster. The following
@@ -36,11 +38,28 @@ TIMESTAMP=`$CURL -X POST \
    |& tee -a $LOG \
    | grep "last-startup" \
    | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
+for i in `seq 1 ${N_RETRY}`; do
+   if [ "$TIMESTAMP" == "" ]; then
+      WARN "Timestart wasnt retrieved. Retry in $RETRY_INTERVAL seconds"
+      sleep $RETRY_INTERVAL
+      TIMESTAMP=`$CURL -X POST \
+         -H "Content-type: application/x-www-form-urlencoded" \
+         --data "admin-username=${USER}" --data-urlencode "admin-password=${PASS}" \
+         --data "realm=${SEC_REALM}" \
+         http://${BOOTSTRAP_HOST}:8001/admin/v1/instance-admin \
+         |& tee -a $LOG \
+         | grep "last-startup" \
+         | sed 's%^.*<last-startup.*>\(.*\)</last-startup>.*$%\1%'`
+   else
+      INFO "Timestamp retrieved"
+      break
+   fi
+done
+
 if [ "$TIMESTAMP" == "" ]; then
   ERROR "Failed to get instance-admin timestamp"
   exit 1
 fi
-
 # Test for successful restart
 INFO "Checking server restart"
 restart_check $BOOTSTRAP_HOST $TIMESTAMP $LINENO
